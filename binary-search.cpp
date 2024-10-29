b063bb75dffef49c858fce7b8907a004cff2042e
Arthur Nunes <arthurvininunes@gmail.com>
2024-10-29 11:00:16 -0300

    Estrutura de Dados 29/10/2024 - TODO

---
+++ binary-search.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int binary_search(int *i, int target, int l, int r)
+{
+    // TODO: Implementar binary_search em C++
+    return -1;
+}
+
+int main()
+{
+    // TODO: Manipular binary_search para qualquer vetor de entrada e qualquer target
+    return 0;
+}
