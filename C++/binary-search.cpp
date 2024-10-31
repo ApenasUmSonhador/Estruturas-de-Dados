2d3375b4f70a18159f93db6ac9092174f0d117db
Arthur Nunes <arthurvininunes@gmail.com>
2024-10-31 10:16:02 -0300

    Implementação Binary Search em C++

--- C++/binary-search.cpp
+++ C++/binary-search.cpp
@@ -1,14 +1,37 @@
 #include <iostream>
 using namespace std;
 
-int binary_search(int *i, int target, int l, int r)
+int binary_search(int vetor[], int target, int r, int l = 0, int i = 0)
 {
-    // TODO: Implementar binary_search em C++
+    if (r >= l)
+    {
+        int mid = l + (r - l) / 2;
+
+        if (vetor[mid] == target)
+        {
+            return mid;
+        }
+
+        if (vetor[mid] > target)
+        {
+            return binary_search(vetor, target, mid - 1, l, i);
+        }
+        return binary_search(vetor, target, r, mid + 1, i + mid + 1);
+    }
     return -1;
 }
 
 int main()
 {
-    // TODO: Manipular binary_search para qualquer vetor de entrada e qualquer target
+    int n, target;
+    cin >> n;
+    int vetor[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> vetor[i];
+    }
+    cin >> target;
+    int result = binary_search(vetor, target, n - 1);
+    cout << result << endl;
     return 0;
 }
